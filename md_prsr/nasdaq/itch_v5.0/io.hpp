00b2ae7b8ffa0abaaab23c4bbedfe8292bc0d263
serge <serge.klim@outlook.com>
2025-06-07 22:04:11 +0100

    timestamp io

--- md_prsr/nasdaq/itch_v5.0/io.hpp
+++ md_prsr/nasdaq/itch_v5.0/io.hpp
@@ -2,13 +2,18 @@
 #include "messages.hpp"
 #include "transcoder/io.hpp"
 #include <iostream>
+#include <chrono>
 
 namespace tc { inline namespace v1 { namespace io {
 
 template<>
 struct formatter<nasdaq::itch::v5_0::timestamp_t, std::true_type> {
     std::ostream& operator()(std::ostream& out, nasdaq::itch::v5_0::timestamp_t const& v) const {
-      out << v.value;
+#ifdef __cpp_lib_format
+       out << std::chrono::hh_mm_ss{std::chrono::nanoseconds { v.value }};
+#else
+       out << v.value;
+#endif // __cpp_lib_format
       return out;
    }
 };
